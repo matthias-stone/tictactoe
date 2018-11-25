14b79286383fc9efccb88c0253299a9e81a376c3
Matthias Stone <matthias@bellstone.ca>
2018-11-25 16:43:03 -0700

    Add more players and a simple game runner

---
+++ cmd/arena/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+
+	"github.com/matthias-stone/tictactoe"
+)
+
+func main() {
+	players := []tictactoe.Player{
+		tictactoe.Random{},
+		tictactoe.RandomOpportunistic{},
+		tictactoe.RandomSpoiler{},
+		tictactoe.RandomOpportunisticSpoiler{},
+	}
+	rand.Seed(time.Now().UnixNano())
+
+	wg := sync.WaitGroup{}
+	for _, p1 := range players {
+		for _, p2 := range players {
+			wg.Add(1)
+			go func(p1, p2 tictactoe.Player) {
+				r := compete(p1, p2, 10000000)
+				fmt.Printf("Draw: %d, %s: %d, %s: %d\n", r[0], p1.Name(), r[1], p2.Name(), r[2])
+				wg.Done()
+			}(p1, p2)
+		}
+	}
+	wg.Wait()
+}
+
+func compete(first, second tictactoe.Player, rounds int) [3]int {
+	var p1, p2 tictactoe.Player
+	results := [3]int{}
+	j := 0
+loop:
+	for j < rounds {
+		j++
+		active, inactive := tictactoe.X, tictactoe.O
+		currentPlayer, nextPlayer := 1, 2
+		if j&1 == 1 {
+			p1, p2 = first, second
+		} else {
+			p1, p2 = second, first
+			currentPlayer, nextPlayer = nextPlayer, currentPlayer
+		}
+
+		gs := tictactoe.GameState(0)
+		for i := 0; i < 9; i++ {
+			m := p1.Move(gs, active)
+			gs |= m
+			if gs.Winner() != tictactoe.Empty {
+				results[currentPlayer] += 1
+				goto loop
+			}
+			p1, p2 = p2, p1
+			active, inactive = inactive, active
+			currentPlayer, nextPlayer = nextPlayer, currentPlayer
+		}
+		results[0] += 1
+	}
+	return results
+}
+
+// func Println(gs tictactoe.GameState) {
+// 	fmt.Printf(strings.Replace(gs.String(), "\n", " ", 2))
+// }
--- random.go
+++ random.go
@@ -2,6 +2,7 @@ package tictactoe
 
 import "math/rand"
 
+// Random makes a random move.
 type Random struct{}
 
 func (r Random) Name() string { return "random" }
@@ -15,6 +16,7 @@ func (r Random) Move(gs, player GameState) GameState {
 	return moves[rand.Int()%len(moves)] & playerMask
 }
 
+// RandomOpportunistic tries to win with the current move, failing that play randomly.
 type RandomOpportunistic struct{}
 
 func (ro RandomOpportunistic) Name() string { return "random-opportunistic" }
@@ -27,6 +29,49 @@ func (ro RandomOpportunistic) Move(gs, player GameState) GameState {
 	moves := gs.AvailableMoves()
 	for _, m := range gs.AvailableMoves() {
 		if ((playerMask & m) | gs).Winner() == player {
+			return m & playerMask
+		}
+	}
+	return moves[rand.Int()%len(moves)] & playerMask
+}
+
+// RandomSpoiler tries to take a winning opportunity from the other player with the current move, failing that play randomly.
+type RandomSpoiler struct{}
+
+func (ro RandomSpoiler) Name() string { return "random-spoiler" }
+
+func (ro RandomSpoiler) Move(gs, player GameState) GameState {
+	playerMask, opponentMask := GameState(allO), GameState(allX)
+	if player == X {
+		playerMask, opponentMask = opponentMask, playerMask
+	}
+	moves := gs.AvailableMoves()
+	for _, m := range gs.AvailableMoves() {
+		if ((opponentMask & m) | gs).Winner() == player {
+			return m & playerMask
+		}
+	}
+	return moves[rand.Int()%len(moves)] & playerMask
+}
+
+// RandomOpportunisticSpoiler tries to win with the current move, failing that tries to take a winning opportunity from the other player with the current move, failing that play randomly.
+type RandomOpportunisticSpoiler struct{}
+
+func (ro RandomOpportunisticSpoiler) Name() string { return "random-opportunistic-spoiler" }
+
+func (ro RandomOpportunisticSpoiler) Move(gs, player GameState) GameState {
+	playerMask, opponentMask := GameState(allO), GameState(allX)
+	if player == X {
+		playerMask, opponentMask = opponentMask, playerMask
+	}
+	moves := gs.AvailableMoves()
+	for _, m := range gs.AvailableMoves() {
+		if ((playerMask & m) | gs).Winner() == player {
+			return m & playerMask
+		}
+	}
+	for _, m := range gs.AvailableMoves() {
+		if ((opponentMask & m) | gs).Winner() == player {
 			return m & playerMask
 		}
 	}
