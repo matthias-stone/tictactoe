d5bd2921f2588935bf2cc7b668fd7fe7c8a8d91d
Matthias Stone <matthias@bellstone.ca>
2018-11-24 19:40:03 -0700

    Deterimine winner

--- gamestate.go
+++ gamestate.go
@@ -2,8 +2,11 @@ package tictactoe
 
 type GameState uint32
 
-const O = 1
-const X = 2
+const (
+	Empty GameState = iota
+	O
+	X
+)
 
 func (gs GameState) String() string {
 	s := make([]byte, 12)
@@ -24,4 +27,56 @@ func (gs GameState) String() string {
 	}
 
 	return string(s)
+}
+
+const (
+	TopMask       = 0x3F
+	MiddleMask    = 0x530
+	BottomMask    = 0x3F000
+	LeftMask      = 0x30C3
+	CenterMask    = 0xC30C
+	RightMask     = 0x30C30
+	AngleDownMask = 0x30303
+	AngleUpMask   = 0x3330
+	allO          = 0x15555
+	allX          = 0x2AAAA
+)
+
+func (gs GameState) Winner() GameState {
+	switch {
+	case gs&TopMask == allO&TopMask:
+		return O
+	case gs&TopMask == allX&TopMask:
+		return X
+	case gs&MiddleMask == allO&MiddleMask:
+		return O
+	case gs&MiddleMask == allX&MiddleMask:
+		return X
+	case gs&BottomMask == allO&BottomMask:
+		return O
+	case gs&BottomMask == allX&BottomMask:
+		return X
+	case gs&LeftMask == allO&LeftMask:
+		return O
+	case gs&LeftMask == allX&LeftMask:
+		return X
+	case gs&CenterMask == allO&CenterMask:
+		return O
+	case gs&CenterMask == allX&CenterMask:
+		return X
+	case gs&RightMask == allO&RightMask:
+		return O
+	case gs&RightMask == allX&RightMask:
+		return X
+	case gs&AngleDownMask == allO&AngleDownMask:
+		return O
+	case gs&AngleDownMask == allX&AngleDownMask:
+		return X
+	case gs&AngleUpMask == allO&AngleUpMask:
+		return O
+	case gs&AngleUpMask == allX&AngleUpMask:
+		return X
+	default:
+		return Empty
+	}
 }
--- gamestate_test.go
+++ gamestate_test.go
@@ -3,7 +3,7 @@ package tictactoe
 import "testing"
 import "github.com/stretchr/testify/assert"
 
-func TestGameStateString(t *testing.T) {
+func TestGameState_String(t *testing.T) {
 	tests := []struct {
 		name   string
 		input  GameState
@@ -12,6 +12,8 @@ func TestGameStateString(t *testing.T) {
 		{"empty", 0, "---\n---\n---\n"},
 		{"centerO", 0x100, "---\n-O-\n---\n"},
 		{"centerX", 0x200, "---\n-X-\n---\n"},
+		{"topLeftO", 0x1, "O--\n---\n---\n"},
+		{"topLeftX", 0x2, "X--\n---\n---\n"},
 		{"intersperced", 0x21212, "X-O\n-X-\nO-X\n"},
 		{"allO", 0x15555, "OOO\nOOO\nOOO\n"},
 		{"allX", 0x2AAAA, "XXX\nXXX\nXXX\n"},
@@ -19,5 +21,34 @@ func TestGameStateString(t *testing.T) {
 	}
 	for _, test := range tests {
 		assert.Equal(t, test.output, test.input.String(), test.name)
+	}
+}
+
+func TestGameState_Winner(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  GameState
+		output GameState
+	}{
+		{"empty", 0, Empty},
+		{"centerO", 0x100, Empty},
+		{"centerX", 0x200, Empty},
+		{"topLeftO", 0x1, Empty},
+		{"topLeftX", 0x2, Empty},
+		{"intersperced", 0x21212, X},
+		{"allO", 0x15555, O},
+		{"allX", 0x2AAAA, X},
+		{"complete", 0x1A966, Empty},
+		{"Top", 0x2A, X},
+		{"Middle", 0x510, O},
+		{"Bottom", 0x2A000, X},
+		{"Left", 0x1041, O},
+		{"Center", 0x8208, X},
+		{"Right", 0x10410, O},
+		{"Angle\\", 0x20202, X},
+		{"Angle/", 0x1110, O},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.output, test.input.Winner(), test.name)
 	}
 }
